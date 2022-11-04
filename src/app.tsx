88d58ca67e140ed09cc4a4a53a34f2d3bdf7343b
Danil <danil@MacBook-Air-Danil.local>
2022-11-04 17:04:20 +0200

    init project

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
+import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -9,6 +10,7 @@ export default function App() {
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default registerRootComponent(App);
