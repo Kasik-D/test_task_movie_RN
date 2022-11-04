f24b3bd0bcdd238c7c59d4dff8dd94d5ec7d624b
Danil <danil@MacBook-Air-Danil.local>
2022-11-04 17:15:53 +0200

    feat(app): configure code-style

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,22 @@
 import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
-}
-
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
     justifyContent: 'center',
   },
 });
